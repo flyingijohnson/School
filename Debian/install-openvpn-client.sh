9f6851c19d5487583e816131200e650fc6d51a8f
Ian Johnson <104862220+flyingijohnson@users.noreply.github.com>
2024-02-07 13:47:57 -0600

    Create install-openvpn-client.sh

---
+++ Debian/install-openvpn-client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is openvpn client install
+# This is for Debian
+# IJ 2/7/2024
+
+sudo apt install network-manager-libreswan-gnome
+sudo apt install openvpn
+sudo apt install network-manager-openvpn
+sudo apt install network-manager-openvpn-gnome
