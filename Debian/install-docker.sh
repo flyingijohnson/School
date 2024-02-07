ddab40dbc693663a4ab7b94e86f9dab0dc3b75f5
Ian Johnson <104862220+flyingijohnson@users.noreply.github.com>
2024-02-07 14:20:26 -0600

    Update install-docker.sh

--- Debian/install-docker.sh
+++ Debian/install-docker.sh
@@ -8,10 +8,11 @@
 # not complete, look at class recording
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
-
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $
 
 # this is in addition to docker documentation
 sudo docker version
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo suermod -aG docker preuss
+sudo usermod -aG docker #username
